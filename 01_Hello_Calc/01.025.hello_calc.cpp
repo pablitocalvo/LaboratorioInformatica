2dbe02db73dd71ee0aab04ef98badcede776ea88
pablitocalvo <sbtpla68@yahoo.it>
2016-10-15 17:48:02 +0200

    aggiunto 01.025.hello...

---
+++ 01_Hello_Calc/01.025.hello_calc.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numero ;
+	
+	numero=3;
+	
+	cout << "numero" << endl;
+	
+	cout << numero << endl;
+	
+	cout << numero + 7 << endl;
+	
+	cout << numero  << endl;
+	
+		 
+}
