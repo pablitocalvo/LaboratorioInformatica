d47bbfd70077cdf186760b513ca53c5c3bddcb47
pablitocalvo <sbtpla68@yahoo.it>
2016-10-21 12:35:38 +0200

    aggiunto 03.200 - 03.220

---
+++ 03_Hello_If/03.200.Hello.if.if.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a=0; 
+	int b = 1;
+	
+	if ( a = 0 )
+	if ( b = 0 ) 
+		 cout <<"xxxx"<<endl;
+	else cout <<"ciao"<<endl;
+		
+		
+	
+	
+}
+
